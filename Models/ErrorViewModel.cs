63c08d9c38b3443eadc795e1d7032db6fd54b889
manoaiharivolaa <manoaiharivola@gmail.com>
2024-06-23 08:24:21 +0300

    Init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnHubFO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
